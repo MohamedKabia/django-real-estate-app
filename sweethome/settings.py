127b21e6908634763710b6484a1ffb22563564f9
simplifyui <simplifyui@gmail.com>
2020-05-17 05:21:46 +0300

    setting.py updated

--- sweethome/settings.py
+++ sweethome/settings.py
@@ -145,3 +145,8 @@ EMAIL_PORT: 587
 EMAIL_HOST_USER: 'kabiaofficial@gmail.com'
 EMAIL_HOST_PASSWORD: 'kabiaofficial'
 EMAIL_USE_TLS: True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
